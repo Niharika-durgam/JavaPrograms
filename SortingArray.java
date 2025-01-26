867f07965c3dd81fc85108d3720e26b9cc29a1ae
Niharika-durgam <niharikadurgam2004@gmail.com>
2025-01-26 12:10:01 +0530

    Add files via upload

---
+++ SortingArray.java
@@ -0,0 +1,26 @@
+//Write a Java program to sort in ascending order an array of given integers[45,35,56,67,78,89,78,12,20]
+
+package PracticePrograms;
+
+// Importing the Arrays class for sorting
+import java.util.Arrays;
+public class SortingArray {
+	public static void main(String[] args) {
+		// Declare and initialize the array with given integers
+		 int[] numbers = {45, 35, 56, 67, 78, 89, 78, 12, 20};
+
+	        // Sorting the array
+	        Arrays.sort(numbers);
+
+	        // Printing the sorted array
+	        System.out.println("Sorted array in ascending order: ");
+	        for (int num : numbers) {
+	            System.out.print(num + " ");
+	        }
+	}
+
+}
+
+/*OUTPUT
+Sorted array in ascending order: 
+12 20 35 45 56 67 78 78 89*/
---
+++ SortingArray2.java
@@ -0,0 +1,22 @@
+//Write a Java program to sort in descending order an array of given string[A,X,D,Z,Y,C,W,B]
+
+package PracticePrograms;
+
+// Importing the Arrays class for sorting
+import java.util.Arrays;
+public class SortingArray2 {
+	public static void main(String[] args) {
+		// Declare and initialize the array with given strings
+		String[] letters = {"A", "X", "D", "Z", "Y", "C", "W", "B"};
+		// Sort and print in descending order
+		Arrays.sort(letters);
+		for (int i = letters.length - 1; i >= 0; i--) {
+			System.out.print(letters[i] + " ");
+		}
+	}
+}
+
+
+/*OUTPUT
+Z Y X W D C B A*/
+
